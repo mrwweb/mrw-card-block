3aa3c592a99eff45163f18396e2a1be6ac12bf01
Mark Root-Wiley <info@mrwweb.com>
2024-05-20 15:18:47 -0700

    overhaul build and package.json to use wp-scripts, eslint, prettier. Ditches CSS build process entirely

---
+++ js/card-block.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-blocks'), 'version' => 'a6497499e455fb0a60ac');
--- mrw-card-block.php
+++ mrw-card-block.php
@@ -33,30 +33,12 @@ function register_block_styles() {
 add_action( 'enqueue_block_assets', __NAMESPACE__ . '\editor_assets' );
 function editor_assets() {
 	if( is_admin() ) {
-		/**
-		 * Ensure the correct dependencies depending on the editor being used
-		 * 
-		 * Thank you Sally CJ! https://wordpress.stackexchange.com/a/413631/9844
-		 */
-		$script_dependencies = array( 'wp-blocks', 'wp-dom-ready' );
-		$screen = get_current_screen();
-		$context = $screen ? $screen->id : '';
-		switch( $context ) {
-			case 'widgets':
-				$script_dependencies[] = 'wp-edit-widgets';
-				break;
-			case 'site-editor':
-				$script_dependencies[] = 'wp-edit-site';
-				break;
-			default:
-				$script_dependencies[] = 'wp-edit-post';
-				break;
-		}
+		$asset_file = include( plugin_dir_path( __FILE__ ) . 'js/card-block.asset.php');
 		
 		wp_enqueue_script(
 			'mrw-card-block-editor-script',
 			plugin_dir_url( __FILE__ ) . 'js/card-block.js',
-			$script_dependencies,
+			$asset_file['dependencies'],
 			MRW_CARD_BLOCK_VERSION
 		);
 	
