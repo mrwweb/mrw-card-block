52856c445ac97c770e07b0c9b571c38b9d1bb7a8
Mark Root-Wiley <info@mrwweb.com>
2024-05-20 14:22:07 -0700

    correctly enqueue assets and script dependcies for various editors. fixes #3

--- mrw-card-block.php
+++ mrw-card-block.php
@@ -5,7 +5,7 @@
  * Author:          Mark Root-Wiley, MRW Web Design
  * Author URI:      https://MRWweb.com
  * Text Domain:     mrw-card-block
- * Version:         0.2.1
+ * Version:         0.2.0
  *
  * @package         Mrw_Card_Block
  */
@@ -17,35 +17,56 @@ use WP_GitHub_Updater;
 
 define( 'MRW_CARD_BLOCK_VERSION', '0.2.0' );
 
-add_action( 'wp_enqueue_scripts', 'MRW\CardBlock\front_end_assets', 9 );
-function front_end_assets() {
-
-	wp_enqueue_style(
-		'mrw-card-block-styles',
-		plugin_dir_url( __FILE__ ) . 'css/card-block-styles.css',
-		[],
-		MRW_CARD_BLOCK_VERSION
+add_filter( 'after_setup_theme', __NAMESPACE__ . '\register_block_styles' );
+function register_block_styles() {
+	wp_enqueue_block_style(
+		"core/media-text",
+		array(
+			'handle' => "mrw-card-block-styles",
+			'src'    => plugin_dir_url( __FILE__ ) . 'css/card-block-styles.css',
+			'path'   => plugin_dir_path( __FILE__ ) . 'css/card-block-styles.css',
+			'ver'	 => MRW_CARD_BLOCK_VERSION,
+		)
 	);
-
 }
 
-add_action( 'enqueue_block_editor_assets', 'MRW\CardBlock\editor_assets' );
+add_action( 'enqueue_block_assets', __NAMESPACE__ . '\editor_assets' );
 function editor_assets() {
-
-	wp_enqueue_script(
-		'mrw-card-block-editor-script',
-		plugin_dir_url( __FILE__ ) . 'js/card-block.js',
-		[],
-		MRW_CARD_BLOCK_VERSION
-	);
-
-	wp_enqueue_style(
-		'mrw-card-block-editor-styles',
-		plugin_dir_url( __FILE__ ) . 'css/card-block-editor-styles.css',
-		[],
-		MRW_CARD_BLOCK_VERSION
-	);
-
+	if( is_admin() ) {
+		/**
+		 * Ensure the correct dependencies depending on the editor being used
+		 * 
+		 * Thank you Sally CJ! https://wordpress.stackexchange.com/a/413631/9844
+		 */
+		$script_dependencies = array( 'wp-blocks', 'wp-dom-ready' );
+		$screen = get_current_screen();
+		$context = $screen ? $screen->id : '';
+		switch( $context ) {
+			case 'widgets':
+				$script_dependencies[] = 'wp-edit-widgets';
+				break;
+			case 'site-editor':
+				$script_dependencies[] = 'wp-edit-site';
+				break;
+			default:
+				$script_dependencies[] = 'wp-edit-post';
+				break;
+		}
+		
+		wp_enqueue_script(
+			'mrw-card-block-editor-script',
+			plugin_dir_url( __FILE__ ) . 'js/card-block.js',
+			$script_dependencies,
+			MRW_CARD_BLOCK_VERSION
+		);
+	
+		wp_enqueue_style(
+			'mrw-card-block-editor-styles',
+			plugin_dir_url( __FILE__ ) . 'css/card-block-editor-styles.css',
+			[],
+			MRW_CARD_BLOCK_VERSION
+		);
+	}
 }
 
 // Initialize the plugin updater
